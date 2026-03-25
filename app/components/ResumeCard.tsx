d12b312f6e918a86dd5fa65d325d845a139d380d
Manjit Kumar Shyanti <manjitkumarshyanti@gmail.com>
2026-03-26 00:32:04 +0530

    Add files via upload

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
+
+export const ResumeCard = ({
+  resume: { id, companyName, jobTitle, feedback, imagePath },
+}: {
+  resume: Resume;
+}) => {
+  const { auth, fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [imagePath]);
+
+  return (
+    <Link
+      to={`/resume/${id}`}
+      className="resume-card animate-in fade-in duration-1000"
+    >
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          {companyName && (
+            <h2 className="text-black! font-bold wrap-break-words">
+              {companyName}
+            </h2>
+          )}
+          {jobTitle && (
+            <h3 className="text-lg wrap-break-word text-gray-500 ">
+              {jobTitle}
+            </h3>
+          )}
+          {!companyName && !jobTitle && (
+            <h2 className="text-black! font-bold">Resume</h2>
+          )}
+        </div>
+        <div className="shrink-0">
+          <ScoreCircle score={feedback.overallScore}></ScoreCircle>
+        </div>
+      </div>
+      {resumeUrl && (
+        <div className="gradient-border animate-in fade-in duration-1000">
+          <div className="w-full h-full ">
+            <img
+              src={resumeUrl}
+              alt="resume"
+              className="w-full h-87.5 max-sm:h-50 object-cover object-top"
+            />
+          </div>
+        </div>
+      )}
+    </Link>
+  );
+};
